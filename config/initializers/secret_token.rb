61b9b6b6fa09f0ad033606fb46b9e6d6ebf17023
Chris Taylor <cctaylor@gmail.com>
2013-11-13 16:27:21 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Madandmain::Application.config.secret_token = 'e98494363f08fa328665313b65f21ab18c812b40430f745c39783fb659a7a0fd763615f6630122d7e803c5c66ad7079f7e984400a4e58ac282247c1257a2db28'
